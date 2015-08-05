5f2c1a2c126ba1d9c759d788ab3f3456d719c7b2
kinglazybones <kinglazybones@gmail.com>
2015-08-05 18:56:18 -0400

    First commit on new branch

---
+++ mygit.rb
@@ -0,0 +1,20 @@
+x = 0
+
+while x < 101
+
+	if x % 5 == 0 && x % 3 == 0
+	puts "Scarlett"
+	elsif x % 5 == 0
+	puts "Logan"
+	elsif x % 3 == 0
+	puts "Gavin"
+	else 
+	puts x
+	
+end
+
+x += 1 
+
+end
+
+
